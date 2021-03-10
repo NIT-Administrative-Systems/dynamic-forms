ccc6f5016c7e355c95134811f2a64daaba899024
Nick Evans <nick.evans@northwestern.edu>
2021-03-09 21:15:25 -0600

    Install vapor-ui

---
+++ app/Providers/VaporUiServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class VaporUiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->gate();
+    }
+
+    /**
+     * Register the Vapor UI gate.
+     *
+     * This gate determines who can access Vapor UI in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewVaporUI', function ($user = null) {
+            // @TODO make this good, but we need more role stuff first!
+            return in_array(optional($user)->username, [
+                'nie7321',
+            ]);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
