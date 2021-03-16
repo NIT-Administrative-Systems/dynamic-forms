ccf447cdfeaf1796bdfd3e34fadca41a9142ae2a
Nick Evans <nick.evans@northwestern.edu>
2021-03-16 14:53:56 -0500

    Reset & seed DB for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('db:seed');
+    }
 }
