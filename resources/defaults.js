cd7694b64990b2b71086634214efaeb7125f1ad7
Nick Evans <nick.evans@northwestern.edu>
2021-03-19 08:44:00 -0500

    Pare down editor (#23)
    * Disable all the inappropriate settings
    * Disable NU purple required indicator

---
+++ resources/defaults.js
@@ -0,0 +1,172 @@
+export default {
+    /**
+     * Merges user-supplied editForm options into the system default editOptions.
+     *
+     * This is a difficult problem, because the data looks like this:
+     *  {
+     *      textarea: [
+     *          {
+     *              key: 'display',
+     *              ignore: false,
+     *              components: [
+     *                  { key: 'foo', ignore: false },
+     *              ],
+     *          }
+     *      ],
+     *  }
+     *
+     * Doing a recursive merge will fall flat on its face when you hit either array, since a general-purpose
+     * recursive or deep merge will merge array key 0 together with the other array key 0, regardless of the key.
+     *
+     * This algorithm is key-aware and merges properly (ish).
+     *
+     * @param object Default options for editForm
+     * @param optionsToMerge User-specified options.editForm
+     * @returns {*}
+     */
+    configMerge: function (object, optionsToMerge) {
+        let path = '';
+        // First level is key:value, unwrap it
+        _.each(optionsToMerge, function (component, componentName) {
+            // Now we're inside the tree
+            _.each(component, function (prop, idx) {
+                let mergeInto = _.findIndex(_.get(object, componentName), function (comp) {
+                    return comp.key === prop.key;
+                });
+
+                if (mergeInto > -1) {
+                    path = componentName + '.' + mergeInto;
+
+                    _.set(object, path, _.mergeWith(_.get(object, path), prop, function (objValue, srcValue, key) {
+                        // @TODO this is going to have the same problem: the components have keys -- make this recursive?
+                        // Although I haven't run into a situation like this yet...so might be OK?
+                        return key === 'components' ? _.concat(objValue, prop.components) : undefined;
+                    }));
+                } else {
+                    object[componentName].push(prop);
+                }
+            });
+        });
+
+        return object;
+    },
+
+    /**
+     * Global defaults applied to all field types
+     */
+    global: _.mapValues(Formio.Components.components, function () {
+        return [
+            { key: 'api', ignore: true },
+            { key: 'logic', ignore: true },
+            { key: 'layout', ignore: true },
+            {
+                key: 'display',
+                ignore: false,
+                components: [
+                    { key: 'customClass', ignore: true },
+                    { key: 'hidden', ignore: true },
+                    { key: 'mask', ignore: true }, // this is the 'Hide Input' checkbox, NOT the input mask field
+                    { key: 'tableView', ignore: true }, // specific to how you view this in the Form.io server admin UI
+                    { key: 'modalEdit', ignore: true }, // specific to how you view this in the Form.io server admin UI (I think)
+                ],
+            },
+            {
+                key: 'data',
+                ignore: false,
+                components: [
+                    { key: 'persistent', ignore: true },
+                    { key: 'protected', ignore: true },
+                    { key: 'dbIndex', ignore: true },
+                    { key: 'encrypted', ignore: true },
+                    { key: 'redrawOn', ignore: true },
+                    { key: 'customDefaultValuePanel', ignore: true },
+                    { key: 'calculateValuePanel', ignore: true },
+                    { key: 'calculateServer', ignore: true },
+                    { key: 'allowCalculateOverride', ignore: true },
+                ],
+            },
+            {
+                key: 'validation',
+                ignore: false,
+                components: [
+                    { key: 'unique', ignore: true },
+                    { key: 'custom-validation-js', ignore: true },
+                    { key: 'json-validation-json', ignore: true },
+                ]
+            },
+            {
+                key: 'conditional',
+                ignore: false,
+                components: [
+                    { key: 'customConditionalPanel', ignore: true },
+                ],
+            }
+        ];
+    }),
+
+    /**
+     * Defaults for specific field types
+     */
+    specificFields: {
+        textarea: [
+            {
+                key: 'display',
+                ignore: false,
+                components: [
+                    { key: 'editor', defaultValue: 'ckeditor', disabled: true }, // do not set hidden, it won't change to ckeditor if you do that
+                    { key: 'wysiwyg', ignore: true },
+                ],
+            },
+            {
+                key: 'data',
+                ignore: false,
+                components: [
+                    { key: 'inputFormat', defaultValue: 'html', disabled: true },
+                ],
+            }
+        ],
+        email: [
+            {
+                key: 'data',
+                ignore: false,
+                components: [
+                    { key: 'inputFormat', ignore: true },
+                ],
+            },
+            {
+                key: 'validation',
+                ignore: false,
+                components: [
+                    { key: 'kickbox', ignore: true },
+                ],
+            },
+        ],
+        address: [
+            {
+                key: 'provider',
+                ignore: false,
+                components: [
+                    { key: 'provider', defaultValue: 'nominatim', disabled: true },
+                    { key: 'manualModeViewString', ignore: true},
+                ],
+            }
+        ],
+        datetime: [
+            {
+                key: 'date',
+                ignore: false,
+                components: [
+                    { key: 'datePicker.disable', ignore: true }, // feature is broken, so it has been disabled
+                ],
+            },
+            {
+                key: 'data',
+                ignore: false,
+                components: [
+                    { key: 'customOptions', ignore: true },
+                    { key: 'defaultDate', ignore: true }, // depends on JS eval. it can work (this JS is client-side-only), but it seems dangerous.
+                ]
+            }
+        ],
+    },
+}
---
+++ resources/index.js
@@ -0,0 +1,34 @@
+import 'formiojs';
+import Defaults from './defaults';
+
+/*
+* We explicitly do NOT support JS evaluation stuff, so disable all of it.
+* This is a limitation of our backend: some of these scripts (probably; assuming)
+* need to run server-side too, to ensure the client isn't doing shenanigans.
+*/
+FormioUtils.Evaluator.noeval = true;
+
+/**
+ * Disable editForm (the modal that pops up when you add/edit a field in the Builder) options that
+ * we do not want to support.
+ *
+ * There are two reasons we do not want to support an option:
+ *
+ *    1. It's too technical for the target audience (don't present choices they don't care about)
+ *    2. It is not supported in the backend (yet, or ever)
+ *
+ * This is achieved by decorating the Formio.builder method with one that sets the editForm options.
+ * To developers working in Blade, this will seem to be happening magically, and will not inhibit their
+ * ability to work with the Formio.builder() method per Form.io's docs.
+ */
+const origFormioBuilder = Formio.builder;
+Formio.builder = function (element, form, options) {
+    options = options || { editForm: {} };
+
+    options.editForm = Defaults.configMerge(
+        Defaults.configMerge(Defaults.global, Defaults.specificFields),
+        options.editForm
+    );
+
+    return origFormioBuilder(element, form, options);
+};
