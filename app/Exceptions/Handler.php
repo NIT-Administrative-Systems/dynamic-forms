16682a787dbed1d5c5543b417d53661145d0f074
Nick Evans <nick.evans@northwestern.edu>
2021-03-26 09:22:33 -0500

    Register Sentry for Laravel handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -33,8 +33,10 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        if (app()->bound('sentry')) {
+            $this->reportable(function (Throwable $e) {
+                app('sentry')->captureException($e);
+            });
+        }
     }
 }
