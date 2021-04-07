7fedcf317328ed1e63892d11606132f0361f411d
Nick Evans <nick.evans@northwestern.edu>
2021-04-07 17:29:07 -0500

    Register a provider for DynamicForms & add a Request macro

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\VaporUiServiceProvider::class,
 
+        \Northwestern\SysDev\DynamicForms\DynamicFormsProvider::class, // @TODO remove me
     ],
 
     /*
