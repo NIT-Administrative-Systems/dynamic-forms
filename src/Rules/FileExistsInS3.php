a4be1f7a5f7b24fad78fe17ba95c332749da58d0
Nick Evans <nick.evans@northwestern.edu>
2021-04-30 14:07:07 -0500

    Detect invalid storage driver in rule & throw exception
    This would indicate something has gone badly wrong on the dev side, so an immediate exception will surface the issue fastest.

--- src/Rules/FileExistsInS3.php
+++ src/Rules/FileExistsInS3.php
@@ -3,6 +3,8 @@
 namespace Northwestern\SysDev\DynamicForms\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Arr;
+use Northwestern\SysDev\DynamicForms\Errors\UnknownStorageDriverError;
 use Northwestern\SysDev\DynamicForms\Storage\StorageInterface;
 
 class FileExistsInS3 implements Rule
@@ -13,20 +15,26 @@ class FileExistsInS3 implements Rule
     public function __construct(
         protected StorageInterface $interface
     ) {
+        //
     }
 
     public function passes($attribute, $value)
     {
-        //Check if all fields exist
-        if (! isset($value) || ! isset($value['storage']) || ! isset($value['name']) || ! isset($value['key'])
-            || ! isset($value['url']) || $value['storage'] != self::STORAGE_S3) {
+        $driverName = Arr::get($value, 'storage');
+        if ($driverName !== self::STORAGE_S3) {
+            throw new UnknownStorageDriverError($driverName);
+        }
+
+        // Check if all fields exist
+        if (! isset($value) || ! isset($value['name']) || ! isset($value['key']) || ! isset($value['url'])) {
             return false;
         }
         //Check consistency of fields
         if ($value['name'] != $value['key'] || (url('/storage/s3/').'/'.$value['name']) != $value['url']) {
             return false;
         }
-        //Check if file exists
+
+        // Check if file exists
         return $this->interface->findObject($value['name']);
     }
 
