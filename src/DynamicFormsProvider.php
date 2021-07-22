392ad6b94a814a50335094609661e242e33669a1
Nick Evans <nick.evans@northwestern.edu>
2021-07-22 10:19:22 -0500

    No slash prepend needed for namespaces

--- src/DynamicFormsProvider.php
+++ src/DynamicFormsProvider.php
@@ -22,8 +22,7 @@ class DynamicFormsProvider extends ServiceProvider
             return new FileComponentRegistry;
         });
 
-        // This is being requested w/ the leading slash.
-        $this->app->singleton('\\'.S3Driver::class, function ($app) {
+        $this->app->singleton(S3Driver::class, function ($app) {
             $clientConfig = [
                 'region' => config('filesystems.disks.s3.region', Arr::get($_ENV, 'AWS_DEFAULT_REGION')),
                 'version' => 'latest',
