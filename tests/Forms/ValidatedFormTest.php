fe2d9e2b70917f2cb6146d7dd061baf6267ba92a
sfk571 <saood.karim@northwestern.edu>
2021-07-27 15:57:52 -0500

    added unit test

--- tests/Forms/ValidatedFormTest.php
+++ tests/Forms/ValidatedFormTest.php
@@ -91,6 +91,7 @@ class ValidatedFormTest extends TestCase
         $validatedForm = new ValidatedForm($flatComponents, $submission);
 
         $this->assertEquals($passes, $validatedForm->isValid());
+        $this->assertEquals($expectedValues, $validatedForm->values());
     }
 
     public function validationDataProvider(): array
@@ -147,6 +148,31 @@ class ValidatedFormTest extends TestCase
                 ['test' => 'lowercase'],
                 true,
                 ['test' => 'LOWERCASE'],
+            ],
+        ];
+    }
+
+    /**
+     * @covers ::allFiles
+     * @dataProvider  filesDataProvider
+     */
+    public function testallFiles(array $flatComponents, array $submission, array $expectedAllfiles): void
+    {
+        $validatedForm = new ValidatedForm($flatComponents, $submission);
+        $this->assertEquals($expectedAllfiles, $validatedForm->allFiles());
+    }
+
+    public function filesDataProvider(): array
+    {
+        $json = fn (string $filename) => file_get_contents(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Fixtures', $filename]));
+        $components = fn (string $filename) => (new Form($json($filename)))->flatComponents();
+        $values = fn (string $filename) => json_decode($json($filename), true);
+
+        return [
+            'allFiles files' => [
+                $components('file_definition.json'),
+                $values('file_submission.json'),
+                [["TEST26-a1d3ce37-c09f-411b-a5e0-58bc4251f489.pdf","TEST26.pdf"],["TEST27-f4982cc1-2395-4f14-9d3d-02864fc2a1ff.pdf","TEST27.pdf"],["TEST28-9ccd7055-782f-4b73-b193-f9ca703013f9.pdf","TEST28.pdf"],["TEST31-1702f647-2375-4a56-9dfa-8683fbc46da3.pdf","TEST31.pdf"],["TEST100-22fffc30-7d24-44bd-aeb4-1dd48aa975e5.pdf","TEST100.pdf"],["TEST99-ae05e075-7b5e-48f3-a495-177fad1ecfd8.pdf","TEST99.pdf"]],
             ],
         ];
     }
