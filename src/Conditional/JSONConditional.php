4f467ec9e6240214955832bf42cfb012fe10650e
Nick Evans <nick.evans@northwestern.edu>
2022-03-22 10:18:11 -0500

    Refactor JSONLogic stuff to live in its own namespace
    This is no longer the domain of just conditionals, so move it somewhere neutral.

--- src/Conditional/JSONConditional.php
+++ src/Conditional/JSONConditional.php
@@ -3,23 +3,12 @@
 namespace Northwestern\SysDev\DynamicForms\Conditional;
 
 use JWadhams\JsonLogic;
+use Northwestern\SysDev\DynamicForms\JSONLogic\JsonLogicHelpers;
 
 class JSONConditional implements ConditionalInterface
 {
-    public function __construct(
-        protected array $jsonLogic,
-    ) {
-        //Replace variable names to be valid in JSON Logic
-        // (may have to do similar for rows if we add support for datagrids)
-        array_walk_recursive(
-            $jsonLogic,
-            function (&$value, $key) {
-                if ($key == 'var' && str_starts_with($value, 'data.')) {
-                    $value = substr($value, 5);
-                }
-            }
-        );
-        $this->jsonLogic = $jsonLogic;
+    public function __construct(array $jsonLogic) {
+        $this->jsonLogic = JsonLogicHelpers::convertDataVars($jsonLogic);
     }
 
     public function __invoke(array $submissionValues): bool
