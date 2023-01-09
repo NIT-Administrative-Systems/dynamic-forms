c700eea306e299829f36dcdd85bae89b9f0de95c
Nick Evans <nick.evans@northwestern.edu>
2023-01-09 08:56:14 -0600

    Expose the action attribute for Buttons (#382)
    This is useful if you want to verify that a form has a submit button.

--- tests/Components/Inputs/ButtonTest.php
+++ tests/Components/Inputs/ButtonTest.php
@@ -11,4 +11,26 @@ use Northwestern\SysDev\DynamicForms\Tests\Components\TestCases\BaseComponentTes
 class ButtonTest extends BaseComponentTestCase
 {
     protected string $componentClass = Button::class;
+
+    /**
+     * @covers ::action
+     *
+     * @dataProvider actionProvider
+     */
+    public function testAction(array $additional, string $expectedAction): void
+    {
+        /** @var Button $component */
+        $component = $this->getComponent(additional: $additional);
+
+        $this->assertEquals($expectedAction, $component->action());
+    }
+
+    public function actionProvider(): array
+    {
+        return [
+            'key not present' => [[], Button::ACTION_SUBMIT],
+            'submit' => [['action' => Button::ACTION_SUBMIT], Button::ACTION_SUBMIT],
+            'save' => [['action' => Button::ACTION_SAVE_STATE], Button::ACTION_SAVE_STATE],
+        ];
+    }
 }
