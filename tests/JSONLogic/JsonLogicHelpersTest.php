d0d8edd74c445c464278f6c36e0e340bf9f97a1b
nie7321 <27235866+nie7321@users.noreply.github.com>
2025-02-24 19:27:07 +0000

    Apply php-cs-fixer changes

--- tests/JSONLogic/JsonLogicHelpersTest.php
+++ tests/JSONLogic/JsonLogicHelpersTest.php
@@ -7,7 +7,6 @@ use Northwestern\SysDev\DynamicForms\JSONLogic\JsonLogicHelpers;
 use Northwestern\SysDev\DynamicForms\JSONLogic\LodashFunctions\___;
 use Northwestern\SysDev\DynamicForms\JSONLogicInitHelper;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\CoversTrait;
 
 #[CoversClass(JsonLogicHelpers::class), CoversClass(JSONLogicInitHelper::class), CoversClass(___::class)]
 final class JsonLogicHelpersTest extends TestCase
