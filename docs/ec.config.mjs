6dae040f4e6624c3d6e80aea954cde5fb90eab01
Danny Foster <danny@northwestern.edu>
2026-04-06 10:25:29 -0500

    chore(deps): bump theme

--- docs/ec.config.mjs
+++
@@ -1,4 +0,0 @@
-import { defineEcConfig } from "@astrojs/starlight/expressive-code";
-import { northwesternExpressiveCode } from "@nu-appdev/northwestern-starlight-theme/expressive-code";
-
-export default defineEcConfig(northwesternExpressiveCode());
